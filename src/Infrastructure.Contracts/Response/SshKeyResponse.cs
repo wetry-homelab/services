490dcd14252eb9d363b027df193422416d14b0b7
Patrick Grasseels <grasseelsp@hotmail.com>
2021-02-04 20:00:06 +0100

    Fix naming

--- src/Infrastructure.Contracts/Response/SshKeyResponse.cs
+++ src/Infrastructure.Contracts/Response/SshKeyResponse.cs
@@ -1,9 +1,16 @@
-﻿namespace Infrastructure.Contracts.Response
+﻿using System.Text.Json.Serialization;
+
+namespace Infrastructure.Contracts.Response
 {
     public class SshKeyResponse
     {
+        [JsonPropertyName("id")]
         public int Id { get; set; }
+        
+        [JsonPropertyName("name")]
         public string Name { get; set; }
+
+        [JsonPropertyName("fingerprint")]
         public string Fingerprint { get; set; }
     }
 }
