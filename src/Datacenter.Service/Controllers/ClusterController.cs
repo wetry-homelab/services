e5007be1c1e1bd0133d0d23cd734e995301a33a5
Patrick Grasseels <grasseelsp@hotmail.com>
2021-02-06 15:39:50 +0100

    Add cluster and instance contracts

---
+++ src/Datacenter.Service/Controllers/ClusterController.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Threading.Tasks;
+
+namespace Datacenter.Service.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ClusterController : ControllerBase
+    {
+        private readonly ILogger<ClusterController> logger;
+
+        public ClusterController(ILogger<ClusterController> logger)
+        {
+            this.logger = logger;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            return Ok();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetOne(int id)
+        {
+            return Ok();
+        }
+
+        [HttpGet("{id}/kubeconfig")]
+        public async Task<IActionResult> GetKubeconfig(int id)
+        {
+            return Ok();
+        }
+
+        [HttpGet("{id}/command/restart")]
+        public async Task<IActionResult> Restart(int id)
+        {
+            return Ok();
+        }
+
+        [HttpGet("{id}/command/{nodeid}/recycle")]
+        public async Task<IActionResult> Recycle(int id)
+        {
+            return Ok();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create()
+        {
+            return Ok();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            return Ok();
+        }
+
+        [HttpPatch("{id}/scale/out")]
+        public async Task<IActionResult> HorizontalScale(int id)
+        {
+            return Ok();
+        }
+
+        [HttpPatch("{id}/scale/up")]
+        public async Task<IActionResult> VerticalScale(int id)
+        {
+            return Ok();
+        }
+    }
+}
---
+++ src/Datacenter.Service/Controllers/InstanceController.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Threading.Tasks;
+
+namespace Datacenter.Service.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class InstanceController : ControllerBase
+    {
+        private readonly ILogger<InstanceController> logger;
+
+        public InstanceController(ILogger<InstanceController> logger)
+        {
+            this.logger = logger;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            return Ok();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetOne(int id)
+        {
+            return Ok();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create()
+        {
+            return Ok();
+        }
+
+        [HttpGet("{id}/snapshot")]
+        public async Task<IActionResult> GetSnapshot(int id)
+        {
+            return Ok();
+        }
+
+        [HttpPost("{id}/snapshot/create")]
+        public async Task<IActionResult> CreateSnapshot(int id)
+        {
+            return Ok();
+        }
+
+        [HttpPut("{id}/snapshot/{snapId}/restore")]
+        public async Task<IActionResult> RestoreSnapshot(int id, Guid snapId)
+        {
+            return Ok();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            return Ok();
+        }
+
+        [HttpPatch("{id}/scale/out")]
+        public async Task<IActionResult> HorizontalScale(int id)
+        {
+            return Ok();
+        }
+
+        [HttpGet("{id}/command/restart")]
+        public async Task<IActionResult> Restart(int id)
+        {
+            return Ok();
+        }
+
+        [HttpGet("{id}/command/start")]
+        public async Task<IActionResult> Start(int id)
+        {
+            return Ok();
+        }
+
+        [HttpGet("{id}/command/stop")]
+        public async Task<IActionResult> Stop(int id)
+        {
+            return Ok();
+        }
+    }
+}
